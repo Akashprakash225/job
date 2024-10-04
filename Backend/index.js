2260b9202e47d403c5f08787f79c05c26e8f93cd
Akashprakash225 <akashprakash225@gmail.com>
2024-10-04 10:00:49 +0530

    cc

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3459", "https://job-frontend-two.vercel.app"],
+    origin: ["http://localhost:3459", "https://job-taupe.vercel.app"],
     credentials: true,
     methods: ["POST", "GET", "DELETE", "UPDATE", "PUT", "OPTIONS"],
   })
